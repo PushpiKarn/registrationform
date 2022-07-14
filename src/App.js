dcd75a67ae85d23f70da2d57225bfb025ca42f58
PushpiKarn <69868555+PushpiKarn@users.noreply.github.com>
2022-07-14 10:46:08 +0530

    SignUp commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Register from './Register';
-
+import Login from '.Login';
 
 //import './App.css';
 function App() {
 
   return (
    <main className ="App">
-    <Register />
+    <Login />
    </main>
   );
 }
